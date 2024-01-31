d140cf04e2dd0a1a4bd0ad8f0ebfe21dd7c024a2
JustFantick <68466135+JustFantick@users.noreply.github.com>
2024-01-31 20:39:56 +0200

    feat: complete JoinUs page

--- src/app/join-us/page.jsx
+++ src/app/join-us/page.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import styles from './page.module.scss'
+import JoinUsForm from '@/components/forms/JoinUsForm'
 
 const JoinUsPage = () => {
 	return (
-		<div>JoinUsPage</div>
+		<main className={styles.joinUsPage}>
+			<h1>Хочете приєнатися?</h1>
+			<p>Заповніть невеличку анкету</p>
+
+			<JoinUsForm />
+
+		</main>
 	)
 }
 
