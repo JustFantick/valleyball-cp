75727c9d9d1f18e2b7a515a53321076f4c27625b
JustFantick <68466135+JustFantick@users.noreply.github.com>
2024-02-01 14:36:44 +0200

    feat: create server actions file

---
+++ src/server-actions/actions.js
@@ -0,0 +1,38 @@
+'use server'
+import prisma from "../../lib/prisma"
+import { revalidatePath } from "next/cache"
+
+export async function addRequestToDb(prevState, formData) {
+	const userClientData = {
+		name: formData.get('name'),
+		email: formData.get('email'),
+		age: formData.get('age'),
+		group: formData.get('group'),
+	}
+
+	revalidatePath('/achievements');
+
+	try {
+		const createdRow = await prisma.players.create({
+			data: {
+				name: userClientData.name,
+				email: userClientData.email,
+				group: userClientData.group,
+			}
+		});
+
+		return { success: true, message: `Заявку на ім'я ${userClientData.name} успішно прийнято!` };
+
+	} catch (e) {
+		return { success: false, error: e, message: `Сталася помилка на боці серверу :(` }
+	}
+}
+
+export async function contactUsFormAction(prevState, formData) {
+	await setTimeout(() => { }, 1000);
+
+	return {
+		success: true,
+		message: "Ваше повідомлення успішно надіслано, наша служба підтримки зв'яжеться з вами найближчим часом"
+	};
+}
